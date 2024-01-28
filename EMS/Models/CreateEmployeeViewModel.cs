228dc87b092231085ab400b33e3470cc20808035
Ayush Pakhrin <pakhrinayush56@gmail.com>
2024-01-28 19:42:12 +0545

    made employee and department form

---
+++ EMS/Models/CreateEmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EMS.Models
+{
+    public class CreateEmployeeViewModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set;} = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; }=string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int? Gender { get; set; }
+
+    }
+}
---
+++ EMS/Models/SetUpDepartmentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EMS.Models
+{
+    public class SetUpDepartmentViewModel
+    {
+        public int? DeptId {  get; set; }
+        public string DeptName { get; set; } = string.Empty;
+        public int? DisplayOrder { get; set;}
+    }
+}
